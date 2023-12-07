dc2186b3b6cc89473dbf6e02301c4022c160ef98
Sol <slereah@gmail.com>
2023-12-07 10:19:09 +0100

    credentials sol funcionando

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
-// const PORT = Number(process.env.PORT) || 45009;
-// const ENVIRONMENT = process.env.ENVIRONMENT;
-// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "";
-// const ADMIN_SDK = JSON.parse(process.env.ADMIN_SDK as string);
-// const PEPPER = process.env.PEPPER;
-// export { PORT, ENVIRONMENT, JWT_SECRET_KEY, ADMIN_SDK, PEPPER };
+import dotenv from "dotenv"
+dotenv.config()
+import "./index"
+
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS as string)
+
+export { CREDENTIALS }
