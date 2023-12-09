a1a8bd0753434c50e6b8f7e85434be06a4f58bb1
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-10 00:14:16 +0100

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,23 +1,17 @@
-import { Router } from "express"
+import { Router } from 'express';
 // import UserController from '../controllers/user';
-// //import MovementController from '../controllers/movement';
-// import { authorizeUser } from '../middlewares/token-validator';
-// import { handle405Error } from '../middlewares/wrong-method-handler';
+import { authorizeUser } from '../middlewares/token-validator';
+import { handle405Error } from '../middlewares/wrong-method-handler';
 
-const userRouter = Router()
+ const userRouter = Router();
 
 // userRouter.post('/', UserController.createNew);
 // userRouter.post('/auth/token', UserController.login);
 
 // userRouter.get('/me', authorizeUser, UserController.getInfo);
 
-// // userRouter.get('/me/movements', authorizeUser, MovementController.getAll);
+userRouter.all('/', handle405Error);
+userRouter.all('/auth/token', handle405Error);
+userRouter.all('/me', handle405Error);
 
-// // userRouter.post('/me/movements', authorizeUser, MovementController.createNew);
-
-// userRouter.all('/', handle405Error);
-// userRouter.all('/auth/token', handle405Error);
-// userRouter.all('/me', handle405Error);
-// //userRouter.all('/me/movements', handle405Error);
-
-export default userRouter
+export default userRouter;
