6841698f9418c5789efe2b8df9a1be95bc86962d
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-10 23:12:47 +0100

    Update controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,47 +1,56 @@
-import { Request, Response } from 'express';
-import UserModel from '../models/user';
+import { Request, Response } from "express"
+import User from "../models/user"
 
 class UserController {
-  static async createUser(req: any, res: any) {
-    const { username, fullname, password, email, birthdate, nationality } = req.body;
+  static async createUser(req: Request, res: Response) {
+    const { username, fullname, password, email, birthdate, nationality } =
+      req.body
     try {
-      await UserModel.createUser({
+      const newUser = await User.create({
         username,
         fullname,
         password,
         email,
         birthdate,
         nationality,
-      });
-      res.status(201).json({ message: 'User created successfully' });
+      })
+      res.status(201).json(newUser)
     } catch (error) {
-      res.status(500).json({ error: 'Error creating user' });
+      res.status(500).json({ error: "Error creating user" })
     }
   }
 
-  static async getUserInfo(req: any, res: any) {
-    const userId = req.params.id;
+  static async getUserInfo(req: Request, res: Response) {
+    const userId = req.params.id
     try {
-      const userInfo = await UserModel.getUserInfo(userId);
-      res.status(200).json(userInfo);
+      const userInfo = await User.findByPk(userId)
+      res.status(200).json(userInfo)
     } catch (error) {
-      res.status(404).json({ error: 'User not found' });
+      res.status(404).json({ error: "User not found" })
+    }
+  }
+  static async getAll(req: Request, res: Response) {
+    try {
+      const users = await User.findAll()
+      res.status(200).json(users)
+    } catch (error) {
+      res.status(404).json({ error: "Users not found" })
     }
   }
 
-  // static async loginUser(req: any, res: any) {
-  //   const { email, password } = req.body;
-  //   try {
-  //     const loginResult = await UserModel.login({ email, password });
-  //     if (loginResult.userInfo) {
-  //       res.status(200).json(loginResult);
-  //     } else {
-  //       res.status(401).json({ error: 'Incorrect credentials' });
-  //     }
-  //   } catch (error) {
-  //     res.status(500).json({ error: 'Authentication failed' });
-  //   }
-  // }
+  static async loginUser(req: any, res: any) {
+    const { email, password } = req.body
+    try {
+      const loginResult = await User.login({ email, password })
+      if (loginResult.userInfo) {
+        res.status(200).json(loginResult)
+      } else {
+        res.status(401).json({ error: "Incorrect credentials" })
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Authentication failed" })
+    }
+  }
 }
 
-export default UserController;
+export default UserController
