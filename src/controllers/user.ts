443666824c4ecd0375dfec16e59a62294ecdb6f4
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-12 16:45:26 +0100

    Update user.ts

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import User from "../models/user"
-import { validateUser, validatePartialUser } from '../schemas/user';
+import { validateUser, validatePartialUser } from "../schemas/user"
 
 class UserController {
   static async createUser(req: Request, res: Response) {
@@ -25,19 +25,21 @@ class UserController {
         birthdate,
         nationality,
       })
-      res.status(201).json(newUser)
+      return res.status(201).json(newUser)
     } catch (error) {
-      res.status(500).json({ error: "Error creating user" })
+      return res.status(500).json({ error: "Error creating user" })
     }
   }
 
   static async getUserInfo(req: Request, res: Response) {
-    const userId = req.params.id
+    const { id } = req.params
     try {
-      const userInfo = await User.findByPk(userId)
-      res.status(200).json(userInfo)
+      const userInfo = await User.findByPk(id)
+      if (!userInfo) {
+        return res.status(200).json(userInfo)
+      }
     } catch (error) {
-      res.status(404).json({ error: "User not found" })
+      return res.status(404).json({ error: "User not found" })
     }
   }
 
@@ -54,51 +56,51 @@ class UserController {
     try {
       const { id } = req.params
       const { body } = req
-  
+
       const user = await User.findByPk(id)
       if (!user) {
         return res.status(404).json({ message: "User not found" })
       }
       await user.update({ body })
       await user.save()
-  
+
       res.json(user)
     } catch (error: any) {
       res.status(500).json({ message: error.message })
     }
   }
-    
-	static async login(req: Request, res: Response) {
-    try {
-      const validatedUser = validatePartialUser(req.body);
-      if (!validatedUser.success)
-			  return res.status(400).json(validatedUser.error);
 
-      const { email, password } = validatedUser.data as any;
-      const loginUser = await User.login({ email, password });
-      if (!loginUser.success) {
-        res.status(200).json(loginUser);
-        return res.json(loginUser);
+  static async login(req: Request, res: Response) {
+    try {
+      const validatedUser = validatePartialUser(req.body)
+      if (!validatedUser.success)
+        return res.status(400).json(validatedUser.error)
+
+      const { email, password } = validatedUser.data as any
+      const loginUser = await User.findOne({ where: { email } })
+      if (loginUser) {
+        res.status(200).json(loginUser)
+        return res.json(loginUser)
       }
     } catch (error) {
-      res.status(500).json({ error: 'Error en la autenticación' });
-    }  
-	}
+      res.status(500).json({ error: "Wrong credentials" })
+    }
+  }
 
   // static async logout(req: Request, res: Response) {
- 	// }
+  // }
 
   static async deleteUser(req: Request, res: Response) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
       await User.destroy({
         where: {
           id,
         },
-      });
-      res.sendStatus(204);
+      })
+      return res.sendStatus(204)
     } catch (error: any) {
-      return res.status(500).json({ message: error.message  });
+      return res.status(500).json({ message: error.message })
     }
   }
 }
