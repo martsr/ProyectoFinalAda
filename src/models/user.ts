eed2bbbcc92680dc2e8c34952415938fe28d8dba
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-12 16:46:21 +0100

    Update user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,45 +1,39 @@
-import { DataTypes, Model } from "sequelize";
-import Sequelize from "sequelize";
-import sequelize from "./database";
-import { string } from "zod";
-import { generateAccessToken, verifiyToken } from "../utils/jwt";
-import {
-  getSalt,
-  hashSeasonPassword,
-  compareHashes,
-} from "../utils/password-hasher";
+import { DataTypes, Model } from "sequelize"
+const { STRING, DATE, UUID } = DataTypes
+import Sequelize from "sequelize"
+import sequelize from "./database"
 
 class User extends Model {}
 
 User.init(
   {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: Sequelize.UUIDV4,
       primaryKey: true,
+      type: UUID,
+      defaultValue: Sequelize.UUIDV4,
     },
     username: {
-      type: DataTypes.STRING,
+      type: STRING,
       allowNull: false,
     },
     fullname: {
-      type: DataTypes.STRING,
+      type: STRING,
       allowNull: false,
     },
     password: {
-      type: DataTypes.STRING,
+      type: STRING,
       allowNull: false,
     },
     email: {
-      type: DataTypes.STRING,
+      type: STRING,
       allowNull: false,
     },
     birthdate: {
-      type: DataTypes.DATE,
+      type: DATE,
       allowNull: false,
     },
     nationality: {
-      type: DataTypes.STRING,
+      type: STRING,
       allowNull: false,
     },
   },
@@ -49,76 +43,7 @@ User.init(
     tableName: "Users",
     timestamps: false,
   }
-);
-(async () => await User.sync({ alter: true }))();
+)
+;(async () => await User.sync({ alter: true }))()
 
-abstract class UserModel {
-  static createUser = async (userData: any) => {
-    const { username, fullname, password, email, birthdate, nationality } =
-      userData;
-    const salt = getSalt();
-    const hashedSeasonPassword = hashSeasonPassword(password, salt);
-    try {
-      const newUser = await User.create({
-        username,
-        fullname,
-        hashedSeasonPassword,
-        email,
-        birthdate,
-        nationality,
-      });
-      console.log("User created successfully:", newUser.toJSON());
-    } catch (error) {
-      console.error("Error creating user:", error);
-    } finally {
-      sequelize.close();
-    }
-  };
-  static getUserInfo = async (userId: string) => {
-    try {
-      const userFound = await User.findByPk(userId);
-
-      if (userFound) {
-        return { userInfo: userFound.toJSON() };
-      } else {
-        throw new Error("User not found");
-      }
-    } catch (error) {
-      throw error;
-    }
-  };
-  //  static async login(userCredentials: any) {
-  //     //falta terminar
-  //     try {
-  //       const { email, password } = userCredentials
-
-  //       const user = await User.findOne({
-  //         where: {
-  //           email: email,
-  //         },
-  //       })
-
-  // if (!user) {
-  //   throw new Error("User not found")
-  // }
-
-  //   const hashedPassword = await User.findOne({where: {
-  //     password: password,
-  //   },})
-
-  //   const passwordMatch = compareHashes( hashedPassword , password);
-
-  //   if (passwordMatch) {
-  //     return {
-  //      message: '¡Usuario autenticado correctamente!',
-  //      userInfo: user.toJSON(),
-  //   };
-  //  }
-
-  //  return { error: 'Credenciales incorrectas' };
-  //  } catch (error) {
-  //   throw error;
-  // }
-}
-
-export default UserModel;
+export default User
