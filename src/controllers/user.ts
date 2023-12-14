a826d3b8e89b74f69ae164b72ae9378eda463058
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-14 13:19:02 +0100

    Update user.ts

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express"
 import User from "../models/user"
-import { validateUser, validatePartialUser } from '../schemas/user';
-import { string } from "zod";
-import { getSalt, hashSeasonPassword, compareHashes } from '../utils/password-hasher';
+import Auth from "../models/auth"
+import { validateUser, validatePartialUser } from "../schemas/user"
+import { string } from "zod"
+import {
+  getSalt,
+  hashSeasonPassword,
+  compareHashes,
+} from "../utils/password-hasher"
 
 class UserController {
   static async createUser(req: Request, res: Response) {
@@ -14,30 +19,39 @@ class UserController {
           email,
         },
       })
+
       if (emailExist) {
         return res
           .status(400)
-          .json({ message: "The email already exist" + email })
+          .json({ message: "The email already exists: " + email })
       }
+
       const newUser = await User.create({
         username,
         fullname,
-        password,
         email,
         birthdate,
         nationality,
       })
-      res.status(201).json(newUser)
+
+      const hashedPassword = await this.createPassword(password)
+      const newAuth = await Auth.create({
+        userId: newUser.id,
+        password: hashedPassword,
+      })
+
+      res.status(201).json(newUser.id)
     } catch (error) {
+      console.error(error)
       res.status(500).json({ error: "Error creating user" })
     }
   }
 
   private static async createPassword(password: string) {
-    const salt = getSalt();
-    const hashPassword = hashSeasonPassword(password, salt);
-    const hashedPassword = `${salt}:${hashPassword}`;
-      return hashedPassword;
+    const salt = getSalt()
+    const hashPassword = hashSeasonPassword(password, salt)
+    const hashedPassword = `${salt}:${hashPassword}`
+    return hashedPassword
   }
 
   static async getUserInfo(req: Request, res: Response) {
@@ -63,48 +77,47 @@ class UserController {
     try {
       const { id } = req.params
       const { body } = req
-  
+
       const user = await User.findByPk(id)
       if (!user) {
         return res.status(404).json({ message: "User not found" })
       }
       await user.update({ body })
       await user.save()
-  
+
       res.json(user)
     } catch (error: any) {
       res.status(500).json({ message: error.message })
     }
   }
-    
-	static async login(req: Request, res: Response) {
+
+  static async login(req: Request, res: Response) {
     try {
-      const { email, password } = req.body;
-      const validatedUser = validatePartialUser(req.body);
+      const { email, password } = req.body
+      const validatedUser = validatePartialUser(req.body)
       if (!validatedUser.success)
-		    return res.status(400).json(validatedUser.error);
-  
-      const user = await User.findOne({ where: { email } });
+        return res.status(400).json(validatedUser.error)
+
+      const user = await User.findOne({ where: { email } })
       const hashedPassword = getSalt()
-      if (!User || !user.compareHashes(password)) {
-        // return res.status(401).json({ error: 'Invalid credentials' });
-      }
+      //if (!User || !user.compareHashes(password)) {
+      // return res.status(401).json({ error: 'Invalid credentials' });}
     } catch (error) {
-      res.status(500).json({ error: 'Authentication failed' });
-    } 
+      res.status(500).json({ error: "Authentication failed" })
+    }
   }
 
   static async deleteUser(req: Request, res: Response) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
       await User.destroy({
         where: {
           id,
         },
-      });
-      res.sendStatus(204);
+      })
+      res.sendStatus(204)
     } catch (error: any) {
-      return res.status(500).json({ message: error.message  });
+      return res.status(500).json({ message: error.message })
     }
   }
 }
