9c17b02db0ec9e8b02a5e4d77a16ea9abc9e460d
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-14 13:20:58 +0100

    Update auth.ts

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,1 +1,30 @@
+import { DataTypes, Model } from "sequelize"
+const { STRING } = DataTypes
+import Sequelize from "sequelize"
+import sequelize from "./database"
+import User from "./user"
+class Auth extends Model {}
+Auth.init(
+  {
+    userId: {
+      primaryKey: false,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Auth",
+    tableName: "Auths",
+    timestamps: false,
+  }
+)
+Auth.belongsTo(User, { foreignKey: "userId" })
+;(async () => await Auth.sync({ alter: true }))()
+
+export default Auth
 
