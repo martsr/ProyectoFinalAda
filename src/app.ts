e2a30841cd6e83369c3be447af27d3d2082a3859
martsr <martinarivero@gmail.com>
2023-12-15 21:24:08 +0100

    installed cookie parser

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express, { json } from "express";
 import { logRequest } from "./middlewares/requests-logger";
 import mainRouter from "./routes";
 import { handle404Error } from "./middlewares/wrong-url-handler";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(json());
+app.use(cookieParser());
 //app.use(logRequest)
 
 app.use("/v1/api", mainRouter);
