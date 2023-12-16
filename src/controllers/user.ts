4411a657a96ff6ec4aeb1bd51c750e79993d51fb
Verónica H. Espejo <139007647+Hxrusoul@users.noreply.github.com>
2023-12-16 14:47:15 -0300

    Complete: Controllet.

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,82 +1,28 @@
 import { Request, Response } from "express";
-import User from "../models/user";
 import Auth from "../models/auth";
-import { validateUser, validatePartialUser } from "../schemas/user";
-import { string } from "zod";
-import {
-  getSalt,
-  hashSeasonPassword,
-  compareHashes,
-} from "../utils/password-hasher";
-import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+import UserModel from "../models/user";
+import { validateUser } from "../schemas/user";
 
 class UserController {
-  private static async createPassword(password: string) {
-    const salt = getSalt();
-    const hashPassword = hashSeasonPassword(password, salt);
-    const hashedPassword = `${salt}:${hashPassword}`;
-    return hashedPassword;
-  }
-
   static async createUser(req: Request, res: Response) {
-    const { username, fullname, password, email, nationality } = req.body;
-    let { birthdate } = req.body;
-    birthdate = new Date(birthdate);
-
     try {
-      const validatedUser = validateUser({
-        username,
-        fullname,
-        password,
-        email,
-        nationality,
-        birthdate,
-      });
-      if (!validatedUser.success)
-        return res.status(400).json(validatedUser.error);
-
-      const emailExist = await User.findOne({
-        where: {
-          email,
-        },
-      });
-
-      if (emailExist) {
-        return res
-          .status(400)
-          .json({ message: "The email already exists: " + email });
-      }
-
-      //const hashedPassword = await this.createPassword(password);
-
-      const newUser = await User.create({
-        username,
-        fullname,
-        password,
-        //password: hashedPassword,
-        email,
-        birthdate,
-        nationality,
-      });
-
-      const newAuth = await Auth.create({
-        userId: newUser.dataValues.id,
-        password,
-        // password: hashedPassword,
-      });
-
-      res.status(201).json(newUser.dataValues.id);
+      const validatedData = validateUser(req.body);
+  
+      if (!validatedData.success)
+        return res.status(400).json(validatedData.error);
+  
+      const response = await UserModel.createUser(validatedData.data);
+      return res.status(201).json(response);
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Error creating user" });
+      return res.status(500).json({ error: "Error creating user" });
     }
-  }
+  } 
 
   static async getUserInfo(req: Request, res: Response) {
-    const userId = req.params.id;
+		const userId = req.params.id;
     try {
-      const userInfo = await User.findByPk(userId);
-      res.status(200).json(userInfo);
+		  const user = await UserModel.getUserInfo(userId);
+      res.status(200).json(user);
     } catch (error) {
       res.status(404).json({ error: "User not found" });
     }
@@ -84,122 +30,59 @@ class UserController {
 
   static async getAll(req: Request, res: Response) {
     try {
-      const users = await User.findAll();
+      const users = await UserModel.findAll();
       res.status(200).json(users);
     } catch (error) {
       res.status(404).json({ error: "Users not found" });
     }
-  }
+  }  
 
   static async updateUser(req: Request, res: Response) {
     try {
-      const { username, fullname, password, email, nationality } = req.body;
-      let { birthdate } = req.body;
-      birthdate = new Date(birthdate);
-      const validatedUser = validatePartialUser({
-        username,
-        fullname,
-        password,
-        email,
-        nationality,
-        birthdate,
-      });
-      if (!validatedUser.success)
-        return res.status(400).json(validatedUser.error);
-      const { id } = req.params;
-
-      const user = await User.findByPk(id);
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      await user.update({
-        username,
-        fullname,
-        password,
-        email,
-        nationality,
-        birthdate,
-      });
-      await user.save();
-      if (password) {
-        const auth = await Auth.findOne({
-          where: { userId: user.dataValues.id },
-        });
-        if (auth) {
-          await auth.update({ password });
-          await auth.save();
-        }
-      }
-
-      res.json(user);
-    } catch (error: any) {
-      res.status(500).json({ message: error.message });
+      const userData = req.body;
+      const result = await UserModel.updateUser(userData);
+  
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json({ error: "Error updating user" });
     }
   }
 
   static async login(req: Request, res: Response) {
     try {
-      console.log("LOGIN");
-      const { email, password } = req.body;
-      const validatedUser = validatePartialUser(req.body);
-      if (!validatedUser.success)
-        return res.status(400).json(validatedUser.error);
-
-      const user = await User.findOne({ where: { email } });
-      const hashedPassword = getSalt();
-      if (!user) {
-        return res.status(401).json({ error: "Invalid credentials" });
-      }
-      const auth = await Auth.findOne({
-        where: { userId: user.dataValues.id },
-      });
-      if (!auth) return res.status(401).json({ error: "Invalid credentials" });
-
-      if (password != auth.dataValues.password)
-        return res.status(401).json({ error: "Invalid credentials" });
-
-      const accessToken = generateAccessToken(password);
-      const refreshToken = generateRefreshToken(password);
-      await auth.update({ accessToken, refreshToken });
-      await auth.save();
-      return res
-        .status(200)
-        .json({ accessToken: accessToken, refreshToken: refreshToken });
+      const validatedData = validateUser(req.body);
+  
+      if (!validatedData.success)
+        return res.status(400).json(validatedData.error);
+  
+      const { email, password } = validatedData.data as any;
+      const user = await UserModel.login({ email, password });
+      return res.json(user);
     } catch (error) {
-      res.status(500).json({ error: "Authentication failed" });
+      return res.status(500).json({ error: "Error en el proceso de login" });
     }
   }
-
+  
   static async logout(req: Request, res: Response) {
     try {
       const { id } = req.body;
   
       await Auth.update({ refreshToken: null }, { where: { id } });
-  
       return res.status(200).json({ message: "Logout exitoso" });
     } catch (error) {
       return res.status(500).json({ error: "Error al realizar el logout" });
     }
-  }
-  
+  }  
 
   static async deleteUser(req: Request, res: Response) {
     try {
-      const { id } = req.params;
-      await User.destroy({
-        where: {
-          id,
-        },
-      });
-      await Auth.destroy({
-        where: {
-          userId: id,
-        },
-      });
-      res.status(200).json({ message: "User deleted successfully", id: id });
-    } catch (error: any) {
-      return res.status(500).json({ message: error.message });
+      const { userId } = req.body;
+      await UserModel.deleteUser(userId);
+      return res.status(200).json({ message: "User successfully deleted", id: userId });
+    } catch (error) {
+      return res.status(500).json({ error: "Error deleting user" });
     }
   }
 }
+
 export default UserController;
