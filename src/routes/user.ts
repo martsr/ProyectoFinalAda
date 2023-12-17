77666ef40d84da4700b292b1bab636acd8306e53
Martu <44218209+martsr@users.noreply.github.com>
2023-12-17 21:44:13 +0100

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,6 @@ import UserController from "../controllers/user";
 import { authorizeUser } from "../middlewares/token-validator";
 import { handle405Error } from "../middlewares/wrong-method-handler";
 
-
 const userRouter = Router();
 
 userRouter.post("/", UserController.createUser);
@@ -12,13 +11,13 @@ userRouter.get("/all", UserController.getAll);
 
 userRouter.post("/login", UserController.login);
 
-userRouter.get("/:id", authorizeUser, UserController.getUserInfo);
+userRouter.get("/me", authorizeUser, UserController.getUserInfo);
 
-userRouter.patch("/:id", authorizeUser, UserController.updateUser);
+userRouter.patch("/me", authorizeUser, UserController.updateUser);
 
 userRouter.delete("/logout", authorizeUser, UserController.logout);
 
-userRouter.delete("/:id", authorizeUser, UserController.deleteUser);
+userRouter.delete("/me", authorizeUser, UserController.deleteUser);
 
 userRouter.all("/", handle405Error);
 userRouter.all("/auth/token", handle405Error);
