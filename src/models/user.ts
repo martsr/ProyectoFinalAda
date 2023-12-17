cc3742137fa9b3170fa32455555e7592aa3132ec
Sol Lereah <88149126+Solereah@users.noreply.github.com>
2023-12-17 02:08:20 +0100

    Update user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,68 +1,77 @@
-import { DataTypes, Model } from "./database";
-const { STRING, DATEONLY } = DataTypes;
-import Sequelize from "sequelize";
-import sequelize from "./database";
+import { DataTypes, Model } from "./database"
+const { STRING, DATEONLY, UUID } = DataTypes
+import Sequelize from "sequelize"
+import sequelize from "./database"
+import Auth from "../models/auth"
 import {
   getSalt,
   hashSeasonPassword,
   compareHashes,
-} from "../utils/password-hasher";
-import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
-import Auth from "./auth";
+} from "../utils/password-hasher"
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt"
 
-class UserModel extends Model {
+class User extends Model {
+  static associate(models: any) {
+    User.hasOne(models.Auth, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      as: "auth",
+    })
+  }
   private static async createHashedSeasonPassword(password: string) {
-    const salt = getSalt();
-    const hashPassword = hashSeasonPassword(password, salt);
-    const hashedPassword = `${salt}:${hashPassword}`;
-    return hashedPassword;
+    const salt = getSalt()
+    const hashPassword = hashSeasonPassword(password, salt).toString("hex")
+    const hashedPassword = `${salt}:${hashPassword}`
+    return hashedPassword
   }
 
   static async createUser(userData: any) {
-    const { username, fullname, password, email, nationality } = userData;
+    const { username, fullname, password, email, birthdate, nationality } =
+      userData
 
-    let { birthdate } = userData;
-    birthdate = new Date(birthdate);
-
-    await UserModel.findOne({
+    const existingUser = await User.findOne({
       where: {
         email,
       },
-    });
+    })
+    if (existingUser) {
+      return { error: "User with the provided email already exists" }
+    }
 
-    const hashedPassword = this.createHashedSeasonPassword(password);
+    const hashedPassword = await this.createHashedSeasonPassword(password)
 
-    const newUser = await UserModel.create({
+    const newUser = await User.create({
       username,
       fullname,
       email,
       birthdate,
       nationality,
-    });
-    await Auth.create({
-      userId: newUser.dataValues.id,
+    })
+    await Auth.createAuth({
+      id: newUser.dataValues.id,
       password: hashedPassword,
-    });
+    })
+
     return {
       message: `User ${fullname} created successfully`,
       id: newUser.dataValues.id,
-    };
+    }
   }
   static async getUserInfo(userId: string) {
-    const userInfo = await UserModel.findByPk(userId);
-    return { userInfo };
+    const userInfo = await User.findByPk(userId)
+    return { userInfo }
   }
 
   static async getAll() {
-    const users = await UserModel.findAll({ limit: 10 });
-    return users;
+    const users = await User.findAll({ limit: 10 })
+    return users
   }
   static async updateUser(userData: any) {
-    const { id, username, fullname, password, email, nationality } = userData;
-    let { birthdate } = userData;
-    birthdate = new Date(birthdate);
+    const { id, username, fullname, password, email, nationality } = userData
+    let { birthdate } = userData
+    birthdate = new Date(birthdate)
 
-    const user = await UserModel.findByPk(id);
+    const user = await User.findByPk(id)
     if (user) {
       await user.update({
         username,
@@ -71,68 +80,76 @@ class UserModel extends Model {
         email,
         nationality,
         birthdate,
-      });
-      await user.save();
+      })
+      await user.save()
       if (password) {
         const auth = await Auth.findOne({
           where: { userId: id },
-        });
+        })
         if (auth) {
-          await auth.update({ password });
-          await auth.save();
+          await auth.update({ password })
+          await auth.save()
         }
       }
     }
-    return { message: "User succesfully updated" };
+    return { message: "User succesfully updated" }
   }
   static async login(userCredentials: any) {
-    const { email, password } = userCredentials;
-    const user = await UserModel.findOne({ where: { email } });
+    const { email, password } = userCredentials
+    const user = await User.findOne({ where: { email } })
+
     if (user) {
       const auth = await Auth.findOne({
         where: { userId: user.dataValues.id },
-      });
+      })
+
       if (auth) {
-        const [salt, dbHashedPassword] = auth.dataValues.password.split(":");
-        const hashedPassword = hashSeasonPassword(password, salt);
-        const equalPasswords = compareHashes(dbHashedPassword, hashedPassword);
-
+        const [salt, dbHashedPassword] = await auth.dataValues.password.split(
+          ":"
+        )
+        const hashedPassword = hashSeasonPassword(password, salt)
+        const equalPasswords = compareHashes(dbHashedPassword, hashedPassword)
+        console.log(equalPasswords)
         if (equalPasswords) {
-          const accessToken = generateAccessToken(password);
-          const refreshToken = generateRefreshToken(password);
+          console.log("entre al if")
+          console.log(generateRefreshToken(password))
+          const refreshToken = generateRefreshToken(password)
 
-          await auth.update({ accessToken, refreshToken });
-          await auth.save();
+          const accessToken = generateAccessToken(password)
+          console.log(accessToken)
+
+          await auth.update({ accessToken, refreshToken })
+          await auth.save()
           return {
             message: "User logged successfully!",
             accessToken,
             refreshToken,
-          };
+          }
         }
       }
     }
-    return { error: "Wrong credentials" };
+    return { error: "Wrong credentials" }
   }
 
   static async deleteUser(userId: string) {
-    await UserModel.destroy({
+    await User.destroy({
       where: {
         userId,
       },
-    });
+    })
     await Auth.destroy({
       where: {
         userId: userId,
       },
-    });
-    return { message: "User successfully deleted", id: userId };
+    })
+    return { message: "User successfully deleted", id: userId }
   }
 }
 
-UserModel.init(
+User.init(
   {
     id: {
-      type: Sequelize.UUID,
+      type: UUID,
       primaryKey: true,
       defaultValue: Sequelize.UUIDV4,
     },
@@ -141,10 +158,6 @@ UserModel.init(
       allowNull: false,
     },
     fullname: {
-      type: STRING,
-      allowNull: false,
-    },
-    password: {
       type: STRING,
       allowNull: false,
     },
@@ -167,10 +180,7 @@ UserModel.init(
     tableName: "Users",
     timestamps: false,
   }
-);
-
-UserModel.hasOne(Auth, { foreignKey: "userId" });
-//Auth.belongsTo(UserModel, { foreignKey: "userId" })
-(async () => await UserModel.sync({ alter: true }))();
-//;(async () => await User.drop())()
-export default UserModel;
+)
+;(async () => await User.sync({ alter: true }))()
+//;(async () => await UserModel.drop())()
+export default User
